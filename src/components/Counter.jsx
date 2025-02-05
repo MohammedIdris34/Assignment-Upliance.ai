b66cef3efc75a5e6c5ec368e78632624fd56be91
MohammedIdris34 <mohdidris7862@gmail.com>
2025-02-05 13:30:46 +0530

    Created count state and bgColor state using useState Hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [bgcolor, setBgColor] = useState("bg-blue-100");
     
+    useEffect(()=> {
+        const lightness = Math.max(100 - count * 1.5, 30);
+        setBgColor(`bg-blue-${lightness}`);
+    }, [count])
   return (
     <div>
-
+        
     </div>
   )
 }
